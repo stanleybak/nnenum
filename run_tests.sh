65e2f9ca21eb617b33177e406e3afa9888fd811c
Stanley Bak <stanleybak@gmail.com>
2026-01-08 15:30:13 -0500

    updated bug7

--- run_tests.sh
+++ run_tests.sh
@@ -8,6 +8,8 @@ grep "violated" out.txt
 python3 -m nnenum.nnenum examples/test/test_unsat.onnx examples/test/test_prop.vnnlib 60 out.txt
 grep "holds" out.txt
 
+rm -f out.txt
+
 python3 -m nnenum.nnenum examples/acasxu/data/ACASXU_run2a_1_1_batch_2000.onnx examples/acasxu/data/prop_1.vnnlib
 
 python3 -m nnenum.nnenum examples/mnistfc/mnist-net_256x2.onnx examples/mnistfc/prop_0_0.03.vnnlib
@@ -16,6 +18,8 @@ python3 -m nnenum.nnenum examples/mnistfc/mnist-net_256x2.onnx examples/mnistfc/
 
 python3 -m nnenum.nnenum examples/cifar2020/cifar10_2_255_simplified.onnx examples/cifar2020/cifar10_spec_idx_11_eps_0.00784_n1.vnnlib
 
+# timeout expected
 python3 -m nnenum.nnenum examples/cifar2020/cifar10_2_255_simplified.onnx examples/cifar2020/cifar10_spec_idx_3_eps_0.00784_n1.vnnlib 60 /dev/null
+
 
 echo "Passed all tests."
